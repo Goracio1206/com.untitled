35228b8aa178144d7c5f3c1636fec1a06bb1c44d
Admin <vitalii.nedzelskyi@gmail.com>
2017-02-23 08:37:28 -0800

    Added constructor

--- src/main/java/Entity/Address.java
+++ src/main/java/Entity/Address.java
@@ -6,4 +6,9 @@ package Entity;
 public class Address {
     private String Country;
     private String City;
+
+    public Address(String Country, String City) {
+        this.City = City;
+        this.Country = Country;
+    }
 }
